f19bebcc5dbead16c1522ad635fa0cc0451c6785
Ben <4991309+NuroDev@users.noreply.github.com>
2024-07-28 11:47:39 +0100

    Added `hmrIntegration` to playground example

--- playground/astro.config.ts
+++ playground/astro.config.ts
@@ -1,8 +1,16 @@
-import bun from '@nurodev/astro-bun';
+import { createResolver } from 'astro-integration-kit';
+import { hmrIntegration } from 'astro-integration-kit/dev';
 import { defineConfig } from 'astro/config';
+
+const { default: bun } = await import('@nurodev/astro-bun');
 
 // https://astro.build/config
 export default defineConfig({
   adapter: bun(),
+  integrations: [
+    hmrIntegration({
+      directory: createResolver(import.meta.url).resolve('../package/dist'),
+    }),
+  ],
   output: 'hybrid',
 });
